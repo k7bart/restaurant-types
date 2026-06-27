40793b83e3750ac2cb061c65ee91c815d3f494ae
Kateryna Bartienieva <k7bart@gmail.com>
2026-06-27 22:05:06 +0300

    update types

--- src/event.ts
+++ src/event.ts
@@ -1,8 +1,16 @@
 import type { BaseEntity } from "./common";
+import { Dish } from "./menu";
 
 interface Event extends BaseEntity {
     title: string;
+    subtitle?: string;
     date: Date;
+    ageLimit?: number;
+    language?: string;
+    menu?: Dish[];
+    price?: number;
+    specialGuest?: string;
+    photo?: string;
 }
 
 export type { Event };
